bffe33cbc566ec4c80ff60e1f68e3489423f4906
Mikhail <tekkaruw@gmail.com>
2023-01-01 21:42:32 +0300

    Starting commit for developmment

---
+++ src/main/java/com/shop/ecommerse/ECommerseApplication.java
@@ -0,0 +1,13 @@
+package com.shop.ecommerse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shop/ecommerse/ECommerseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.ecommerse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
